aa937c08a64945644543be4c0061ff2bcc8c7bef
Cristina <cristina@Cristinas-Mac-mini.local>
2020-01-16 22:06:11 +0200

    extract

--- extract.go
+++ extract.go
@@ -2,50 +2,77 @@ package main
 
 import (
 	"regexp"
+	"strconv"
+	"strings"
 )
 
-type info struct {
-	CompanyName string
-	Address     string
-	Jobs        []job
+type company struct {
+	ID      int64
+	Name    string
+	Address string
+	Jobs    []job `bigquery:"-"`
 }
 
 type job struct {
-	Name string
-	Link string
-	City string
+	ID        int64
+	CompanyID int64
+	Name      string
+	Link      string
+	City      string
 }
 
-func extract(content string) info {
-	i := info{}
+// extract extrage inf din pagina
+func extract(companyID int64, content string) (*company, error) {
+	c := company{
+		ID: companyID,
+	}
 
 	rule := regexp.MustCompile(`<p\sclass="profile-company-name">\s*(?P<company>.*)\s*</p>`)
 	matches := rule.FindAllStringSubmatch(content, -1)
-	i.CompanyName = matches[0][1]
 
-	rule = regexp.MustCompile(`<span class="contact-info\scontact-info--adress">\s*<i(?:.*)</i>\s*(.*)\s*</span>`)
-	matches = rule.FindAllStringSubmatch(content, -1)
-	i.Address = matches[0][1]
+	if len(matches) > 0 {
+		c.Name = matches[0][1]
 
-	rule = regexp.MustCompile(`<h2\sclass="jobitem-title"\sitemprop="title">\s*<a(?:.*)>(.*)</a></h2>`)
-	nameMatches := rule.FindAllStringSubmatch(content, -1)
-
-	rule = regexp.MustCompile(`<a\sclass="title\sdataLayerItemLink"\shref="(.*)">(?:.*)</a>\s*</h2>`)
-	linkMatches := rule.FindAllStringSubmatch(content, -1)
-
-	rule = regexp.MustCompile(`<a\shref="javascript:;"\sclass="jobitem-info-item\sjobitem-icon\sjobitem-cities-qtip"\stitle="(.*)"\s*itemprop="jobLocation">`)
-	cityMatches := rule.FindAllStringSubmatch(content, -1)
-
-	jobs := []job{}
-	for index, j := range nameMatches {
-		jb := job{
-			Name: j[1],
-			Link: linkMatches[index][1],
-			City: cityMatches[index][1],
+		rule = regexp.MustCompile(`<span class="contact-info\scontact-info--adress">\s*<i(?:.*)</i>\s*(.*)\s*</span>`)
+		matches = rule.FindAllStringSubmatch(content, -1)
+		if len(matches) > 0 {
+			c.Address = matches[0][1]
 		}
-		jobs = append(jobs, jb)
-	}
-	i.Jobs = jobs
 
-	return i
+		rule = regexp.MustCompile(`<h2\sclass="jobitem-title"\sitemprop="title">\s*<a(?:.*)>(.*)</a></h2>`)
+		nameMatches := rule.FindAllStringSubmatch(content, -1)
+
+		rule = regexp.MustCompile(`<a\sclass="title\sdataLayerItemLink"\shref="(.*)">(?:.*)</a>\s*</h2>`)
+		linkMatches := rule.FindAllStringSubmatch(content, -1)
+
+		rule = regexp.MustCompile(`<a\shref="javascript:;"\sclass="jobitem-info-item\sjobitem-icon\sjobitem-cities-qtip"\stitle="(.*)"\s*itemprop="jobLocation">`)
+		cityMatches := rule.FindAllStringSubmatch(content, -1)
+
+		jobs := []job{}
+
+		for index, j := range nameMatches {
+			link := linkMatches[index][1]
+			list := strings.Split(link, "/")
+			jID := list[len(list)-1]
+
+			jobID, err := strconv.ParseInt(jID, 10, 64)
+			if err != nil {
+				return nil, err
+			}
+
+			jb := job{
+				ID:        jobID,
+				CompanyID: companyID,
+				Name:      j[1],
+				Link:      link,
+				City:      cityMatches[index][1],
+			}
+
+			jobs = append(jobs, jb)
+		}
+
+		c.Jobs = jobs
+	}
+
+	return &c, nil
 }
--- main.go
+++ main.go
@@ -1,14 +1,54 @@
 package main
 
 import (
-	"github.com/davecgh/go-spew/spew"
+	"context"
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/bigquery"
 )
 
+// comanda conectare la big query
+// export GOOGLE_APPLICATION_CREDENTIALS=/Users/cristina/bqkey.json
+
 func main() {
-	content, err := request("https://www.ejobs.ro/company/ejobs-group/266860")
+	ctx := context.Background()
+
+	client, err := bigquery.NewClient(ctx, "onedollarfootage")
 	if err != nil {
 		panic(err)
 	}
-	i := extract(*content)
-	spew.Dump(i)
+
+	cins := client.Dataset("ejobsmap").Table("companies").Inserter()
+	jins := client.Dataset("ejobsmap").Table("jobs").Inserter()
+
+	var i int64
+	for i = 309563; i <= 1000000; i++ {
+		link := fmt.Sprintf("https://www.ejobs.ro/company/ejobs-group/%d", i)
+		content, err := request(link)
+		if err != nil {
+			panic(err)
+		}
+
+		c, err := extract(i, *content)
+		if err != nil {
+			panic(err)
+		}
+
+		if c.Name != "" {
+			log.Println(fmt.Sprintf("%s - %d jobs", c.Name, len(c.Jobs)))
+
+			err = cins.Put(ctx, c)
+			if err != nil {
+				panic(err)
+			}
+
+			if len(c.Jobs) > 0 {
+				err = jins.Put(ctx, c.Jobs)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
 }
